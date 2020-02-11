74e8c7e37044f7a029ece14b908d7a794cb83f91
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-11 21:47:32 +0900

    load local fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,12 +10,6 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          {/* Google Font
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:400,500,700&display=swap"
-          />
-          */}
         </Head>
         <body>
           <Main />
