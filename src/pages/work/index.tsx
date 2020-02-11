69c076399986e81852478a0297e5b09a603cd697
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-12 00:38:58 +0900

    fix: portfolio

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import Layout from '../../components/Layout';
-import Hero from '../../components/Hero';
 import Profile from '../../components/Profile';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    hero: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    heroTitle: {
+      fontWeight: 700,
+      letterSpacing: theme.spacing(0.5),
+      marginBottom: theme.spacing(1),
+    },
+    heroText: {
+      textAlign: 'center',
+      '& > p': {
+        color: '#999',
+        margin: 0,
+      },
+    },
+  }),
+);
+
 const Index: NextPage = () => {
+  const classes = useStyles();
+
   return (
-    <Layout>
-      <Hero />
+    <Layout title="Portfolio | hideyuk1.com">
+      {/* Hero */}
+      <section>
+        <Container maxWidth="md" className={classes.hero}>
+          <div className={classes.heroText}>
+            <Typography component="div" variant="h4" className={classes.heroTitle}>
+              Portfolio
+            </Typography>
+            <p>制作物</p>
+          </div>
+        </Container>
+      </section>
+      {/* End Hero */}
       <Profile />
     </Layout>
   );
