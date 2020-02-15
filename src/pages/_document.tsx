69e1a328eb5f158df244477be5198628d93b8a81
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-15 21:31:17 +0900

    fix: blog

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           {/* Google Fonts */}
           <link
-            href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:400,500,700&display=swap"
+            href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:500,700&display=swap"
             rel="stylesheet"
           />
         </Head>
