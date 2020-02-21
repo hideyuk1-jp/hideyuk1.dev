b808cb7128fe8e2bf8d1470670427d76da832a3a
hideyuk1-jp <hh11028+github@gmail.com>
2020-02-21 23:14:05 +0900

    fix: _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,9 +13,17 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           {/* Google Fonts */}
+          {/*
           <link
             href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:500,700&display=swap"
             rel="stylesheet"
+          />
+          */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                '</script><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:500,700&display=swap" media="print" onload="this.media=\'all\'" /><script>',
+            }}
           />
         </Head>
         <body>
